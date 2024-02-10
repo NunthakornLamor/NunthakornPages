f0323f79e19fe6deb5ff36b54758f10bb1232729
YaKoRMuD <iamyakormud@gmail.com>
2024-02-10 23:30:22 +0700

    add showcase page

---
+++ src/controller.js
@@ -0,0 +1,11 @@
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+
+    if (section) {
+      // Using scrollIntoView for smooth scrolling
+      section.scrollIntoView({ behavior: 'smooth' });
+
+      // If you prefer to use scrollTo without smooth scrolling:
+      // window.scrollTo({ top: section.offsetTop, behavior: 'smooth' });
+    }
+  }
